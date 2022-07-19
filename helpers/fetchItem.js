2fc356e1110aed58bf91b8d281e7595a26b6e930
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2022-07-18 23:39:30 -0300

    requisito 3 realizado

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,11 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (item) => {
+  if (!item) {
+    return new Error('You must provide an url');
+  }
+  const urlApi = `https://api.mercadolibre.com/sites/MLB/search?q=${item}`;
+  const response = await fetch(urlApi);
+  const data = await response.json();
+  return data;
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,26 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
-});
+  it('Teste 1', () => {
+    expect(typeof(fetchItem)).toEqual('function');
+  })
+  
+  it('Teste 2', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  })
+  
+  it('Teste 3', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1615760527');
+  })
+  
+  it('Teste 4', async () => {
+    expect(await fetchItem('MLB1615760527')).toEqual(item);
+  })
+  
+  it('Teste 5', async () => {
+    expect(await fetchItem()).toEqual(new Error("You must provide an url"));
+  })
+  
+  });
